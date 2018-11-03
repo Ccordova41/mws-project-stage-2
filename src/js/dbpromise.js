7226dfb3aa56f5e95de636b6e2a0b060d7c49239
Ccordova41 <ccordova41@gmail.com>
2018-11-03 18:57:07 -0400

    refactor fetchrestaurantsbyid

--- src/js/dbpromise.js
+++ src/js/dbpromise.js
@@ -1,40 +1,46 @@
 import idb from 'idb';
 
 const dbPromise = {
-  db: idb.open('restaurant-reviews-db', 1, function(upgradeDB) {
-    switch (upgradeDB.oldVersion) {
+  // creation and updating of database happens here.
+  db: idb.open('restaurant-reviews-db', 1, function (upgradeDb) {
+    switch (upgradeDb.oldVersion) {
       case 0:
-        upgradeDB.createObjectStore('restaurants', { keyPath: 'id'});
+        upgradeDb.createObjectStore('restaurants', { keyPath: 'id' });
     }
   }),
 
-  putRestaurants(restaurants) {
-    if (!restaurants.push) restaurants = [restaurants];
-    return this.db.then(db => {
-      const store = db.transaction('restaurants', 'readwrite').objectStore('restaurants');
-      Promise.all(restaurants.map(networkRestaurant => {
-        return store.get(networkRestaurant.id).then(idbRestaurant => {
-          if (!idbRestaurant || networkRestaurant.updatedAt > idbRestaurant.updatedAt) {
-            return store.put(networkRestaurant);
-          }
-        });
-      })).then(function () {
-        return store.complete;
+
+  /**
+ * Save a restaurant or array of restaurants into idb, using promises.
+ */
+putRestaurants(restaurants) {
+  if (!restaurants.push) restaurants = [restaurants];
+  return this.db.then(db => {
+    const store = db.transaction('restaurants', 'readwrite').objectStore('restaurants');
+    Promise.all(restaurants.map(networkRestaurant => {
+      return store.get(networkRestaurant.id).then(idbRestaurant => {
+        if (!idbRestaurant || networkRestaurant.updatedAt > idbRestaurant.updatedAt) {
+          return store.put(networkRestaurant);
+        }
       });
+    })).then(function () {
+      return store.complete;
     });
-  }, 
+  });
+},
 
-// get restaurant, by its id, or all stored restaurantsin idb using promises.
-  // if no argument is pased, all restaurants wil be retuened.
+/**
+ * Get a restaurant, by its id, or all stored restaurants in idb using promises.
+ * If no argument is passed, all restaurants will returned.
+ */
+getRestaurants(id = undefined) {
+  return this.db.then(db => {
+    const store = db.transaction('restaurants').objectStore('restaurants');
+    if (id) return store.get(Number(id));
+    return store.getAll();
+  });
+},
 
-  getRestaurants(id = undefined) {
-    return this.db.then(db => {
-      const store = db.transaction('restaurant').objectStore('restaurants');
-      if (id) return store.get(Number(id));
-      return store.getAll();
-    })
-  }
-
-}
+};
 
 export default dbPromise;
